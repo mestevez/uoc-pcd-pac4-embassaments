94174b9574ba84182dab1d6cb3745107be385c70
Marc Estévez Amén <marc.estevez@clarivate.com>
2025-06-03 22:21:33 +0200

    Install pylinte

--- README.md
+++ README.md
@@ -9,6 +9,28 @@ Estudis d'Informàtica, Multimèdia i Telecomunicació
 
 ## Instal·lació
 
+### Creació de l'entorn virtual
+És recomanable crear un entorn virtual per evitar conflictes amb altres projectes.
+Per això necessites tenir instal·lat `virtualenv`. Si no el tens instal·lat, pots fer-ho amb pip:
+```bash
+sudo pip install virtualenv
+```
+Després, crea un entorn virtual al directori del projecte:
+```bash
+virtualenv venv
+```
+I activa l'entorn virtual:
+```bash
+source venv/bin/activate
+```
+A partir d'aquest moment, ens trobarem dins de l'entorn virtual (observeu que el cursor de la consola canvia per indicar que ens trobem dins de l'entorn venv).
+
+### Instal·lació de les dependències
+Instal·la les dependències del projecte amb pip. És recomanable crear un entorn virtual per evitar conflictes amb altres projectes.
+```bash
+pip install -r requirements.txt -r dev-requirements.txt
+```
+
 ## Execució
 
 ## Execució dels tests
@@ -16,3 +38,9 @@ Estudis d'Informàtica, Multimèdia i Telecomunicació
 ## Documentació
 
 ## Lint
+S'utilitza `pylint` per a fer l'anàlisi estàtica del codi.
+Aquest està inclòs a les dependències de desenvolupament, per tant, si has instal·lat les dependències de desenvolupament, ja el tens instal·lat.
+L'execució de `pylint` es pot fer sobre un fitxer o directori concret. Per exemple:
+```bash
+pylint src
+```
--- src/main.py
+++ src/main.py
@@ -1,28 +1,61 @@
+"""
+Fitxer d'entrada per a la pràctica de Python.
+Proporciona una gestió bàsica dels paràmetres d'entrada per executar exercicis,
+o ofereix funcionalitats bàsiques com l'ajuda.
+"""
 import sys
 
 def execute_exercise(num: int):
+    """
+    Executa un exercici de la pràctica de forma individual.
+    :param num: Número de l'exercici a executar.
+    :return:
+    """
     print (f"Execute {num}")
 
 def execute_exercises(num_from: int, num_to: int):
+    """
+    Executa els exercicis de la pràctica dins d'un rang determinat.
+    :param num_from: Número d'inici del rang d'exercicis.
+    :param num_to: Número final del rang d'exercicis.
+    :return:
+    """
     for num in range(num_from, num_to + 1):
         execute_exercise(num)
 
 def show_error(err: str):
+    """
+    Mostra un missatge d'error en els paràmetres d'entrada. Hi afegeix un missatge d'ajuda.
+    :param err: Missatge d'error a mostrar.
+    :return:
+    """
     print (f"{err}. Use -h or --help for help")
 
 def show_help():
+    """
+    Mostra un missatge d'ajuda amb les opcions disponibles.
+    :return:
+    """
     print ("Show Help")
 
-if __name__ == "__main__":
-    if "-h" in sys.argv or "--help" in sys.argv:
+def run(argv: list[str]) -> None:
+    """
+    Gestiona els paràmetres d'entrada i executa les funcions corresponents.
+    :param argv: Llista de paràmetres d'entrada.
+    :return:
+    """
+    if "-h" in argv or "--help" in argv:
         show_help()
-    elif len(sys.argv) > 2 and sys.argv[1] == "-ex":
-        ex_num = sys.argv[2]
+    elif len(argv) > 2 and argv[1] == "-ex":
+        ex_num = argv[2]
         if ex_num.isnumeric() and 1 <= int(ex_num) <= 5:
             execute_exercises(1, int(ex_num))
         else:
             show_error(f"No such exercise number {ex_num}")
-    elif len(sys.argv) <= 1:
+    elif len(argv) <= 1:
         execute_exercises(1, 5)
     else:
         show_error("Unrecognize option")
+
+if __name__ == "__main__":
+    run(sys.argv)
