6a55e4399f0e75c85be26037d510bc9bf925ec41
Marc Estévez Amén <marc.estevez@clarivate.com>
2025-06-05 19:52:51 +0200

    Implement Exercici 1

--- src/modules/exercici1/exercici1.py
+++ src/modules/exercici1/exercici1.py
@@ -1,14 +1,34 @@
 """
 Exercici 1. Load dataset i EDA
 """
+import pandas
 
-def run():
+def run() -> pandas.DataFrame:
     """
     Executa l'exercici 1 de la pràctica.
     :return:
     """
+    print("======================================")
     print("Executing Exercise 1")
-    # Aquí es pot afegir la lògica específica de l'exercici 1
+    print("======================================")
+    print("1.1 - Carrega el dataset en un dataframe de Pandas:")
+    df: pandas.DataFrame = pandas.read_csv("data/Quantitat_d_aigua_als_embassaments_de_les_Conques_Internes_de_Catalunya_20250531.csv")
+    print("\t... Dataset carregat amb èxit\n")
+
+    print("1.2 - Mostra les 5 primeres files:")
+    print(df.head(5))
+    print("\n")
+
+    print("1.3 - Mostra les columnes del dataframe:")
+    for c in df.columns.tolist():
+        print(f"- {c}")
+    print("\n")
+
+    print("1.4 - Mostra la informació (info()):")
+    print(df.info())
+    print("\n")
+
+    return df
 
 if __name__ == "__main__":
     run()
--- tests/test_exercici1.py
+++ tests/test_exercici1.py
@@ -2,17 +2,67 @@
 Tests pel codi corresponent a l'exercici 1.
 """
 import unittest
+import sys
+from io import StringIO
+from modules.exercici1 import exercici1
+
 
 class TestExercise1(unittest.TestCase):
     """
     Classe de test per a l'exercici 1.
     """
 
-    def test_should_run_exercise(self):
+    def test_should_run_exercise_and_return_dataframe(self):
         """
-        Implementar la lógica del test.
+        Executa l'exercici 1 i comprova que retorna un DataFrame.
         """
-        self.assertEqual(True, True)  # add assertion here
+        df = exercici1.run()
+
+        self.assertIsNotNone(df)
+
+    def test_should_run_exercise_and_show_first_rows(self):
+        """
+        Executa l'exercici 1 i comprova que es mostren les primeres files del DataFrame.
+        """
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        exercici1.run()
+
+        sys.stdout = sys.__stdout__
+        self.assertIn("0  30/05/2025", captured_output.getvalue())
+        self.assertIn("1  30/05/2025", captured_output.getvalue())
+        self.assertIn("2  30/05/2025", captured_output.getvalue())
+        self.assertIn("3  30/05/2025", captured_output.getvalue())
+        self.assertIn("4  30/05/2025", captured_output.getvalue())
+
+    def test_should_run_exercise_and_show_all_columns(self):
+        """
+        Executa l'exercici 1 i comprova que es mostren totes les columnes del DataFrame.
+        """
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        exercici1.run()
+
+        sys.stdout = sys.__stdout__
+        self.assertIn("- Dia", captured_output.getvalue())
+        self.assertIn("- Estació", captured_output.getvalue())
+        self.assertIn("- Nivell absolut (msnm)", captured_output.getvalue())
+        self.assertIn("- Percentatge volum embassat (%)", captured_output.getvalue())
+        self.assertIn("- Volum embassat (hm3)", captured_output.getvalue())
+
+    def test_should_run_exercise_and_show_info(self):
+        """
+        Executa l'exercici 1 i comprova que es mostra la informació del DataFrame.
+        """
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        exercici1.run()
+
+        sys.stdout = sys.__stdout__
+        self.assertIn("RangeIndex: 83538 entries, 0 to 83537", captured_output.getvalue())
 
 if __name__ == '__main__':
     unittest.main()
